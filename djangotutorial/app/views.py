2dd04ca400907f52a2a605b5b15d153d5a2d2163
Micael Dali <dali.micael@gmail.com>
2026-05-13 15:02:29 -0300

    Criação dos ViewSets base para todas as classes

--- djangotutorial/app/views.py
+++ djangotutorial/app/views.py
@@ -1,5 +1,72 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import viewsets
+from app.models import (
+    Aluno,
+    Professor,
+    Coordenador,
+    ModeloDocumento,
+    SolicitacaoEstagio,
+    ParecerTecnico,
+    Relatorio,
+    Apolice,
+    Contrato,
+    AssinaturaDigital
+)
+from app.serializers import (
+    AlunoSerializer,
+    ProfessorSerializer,
+    CoordenadorSerializer,
+    ModeloDocumentoSerializer,
+    SolicitacaoEstagioSerializer,
+    ParecerTecnicoSerializer,
+    RelatorioSerializer,
+    ApoliceSerializer,
+    ContratoSerializer,
+    AssinaturaDigitalSerializer
+)
 
-def home(request):
-    return HttpResponse("Bem-vindo ao meu site!")
+class AlunoViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class ProfessorViewSet(viewsets.ModelViewSet):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
+
+class CoordenadorViewSet(viewsets.ModelViewSet):
+    queryset = Coordenador.objects.all()
+    serializer_class = CoordenadorSerializer
+
+
+class ModeloDocumentoViewSet(viewsets.ModelViewSet):
+    queryset = ModeloDocumento.objects.all()
+    serializer_class = ModeloDocumentoSerializer
+
+
+class SolicitacaoEstagioViewSet(viewsets.ModelViewSet):
+    queryset = SolicitacaoEstagio.objects.all()
+    serializer_class = SolicitacaoEstagioSerializer
+
+
+class ParecerTecnicoViewSet(viewsets.ModelViewSet):
+    queryset = ParecerTecnico.objects.all()
+    serializer_class = ParecerTecnicoSerializer
+
+
+class RelatorioViewSet(viewsets.ModelViewSet):
+    queryset = Relatorio.objects.all()
+    serializer_class = RelatorioSerializer
+
+
+class ApoliceViewSet(viewsets.ModelViewSet):
+    queryset = Apolice.objects.all()
+    serializer_class = ApoliceSerializer
+
+
+class ContratoViewSet(viewsets.ModelViewSet):
+    queryset = Contrato.objects.all()
+    serializer_class = ContratoSerializer
+
+
+class AssinaturaDigitalViewSet(viewsets.ModelViewSet):
+    queryset = AssinaturaDigital.objects.all()
+    serializer_class = AssinaturaDigitalSerializer
